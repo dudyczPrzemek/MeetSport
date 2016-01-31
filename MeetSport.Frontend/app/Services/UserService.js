7e4d38ce45533c51eadfbba45506c689a318e5ba
Oskar Dudycz <o.dudycz@lgbs.pl>
2016-01-31 13:11:39 +0100

    Added API unifed class for Webapi usage

--- MeetSport.Frontend/app/Services/UserService.js
+++ MeetSport.Frontend/app/Services/UserService.js
@@ -1,22 +1,7 @@
 ﻿function UserService() {
-
     var self = this;
 
-    function handleLogOut() {
-        toastr.error("Zostałeś wylogowany!", "Błąd");
-        cache.ClearAll();
-        routing.refresh();
-    }
-
-    function handleStandardError(jqXHR, exception) {
-        if (jqXHR.status === 401) {
-            handleLogOut();
-            return true;
-        }
-        return false;
-    }
-
-    var UserAPI = $data.define("User", {
+    var api = new API("User", {
         Id: Number,
         UserName: String,
         FirstName: String,
@@ -24,23 +9,10 @@
         Email: String,
     });
 
-    UserAPI.setStore('default', {
-        provider: 'webApi',
-        dataSource: '/api/user'
-    });
-
-    $data.EntityContext.prototype.prepareRequest = function (r) {
-        r.headers['Authorization'] = "Bearer " + authManager.getToken();
-    };
-    
     self.getUser = function (username, callback) {
-        UserAPI.query(function (user) { return user.UserName === this.username; }, {username: username})
-            .then(function (items) {
-                callback(items[0]);
-            })
-            .fail(function (err) {
-                console.log("item not found");
-            });
+        api.query(function(user) { return user.UserName === this.username; }, { username: username }, function(items) {
+            callback(items[0]);
+        });
     }
 };
 
